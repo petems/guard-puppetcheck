2a78914aa0b5f81608f6b1dbdf949d6da4c9408a
Peter Souter <p.morsou@gmail.com>
2014-02-07 17:31:27 +0000

    A few fixes

--- lib/guard/puppetcheck.rb
+++ lib/guard/puppetcheck.rb
@@ -23,14 +23,6 @@ module Guard
       run_on_change(Watcher.match_files(self, Dir.glob('{,**/}*{,.*}').uniq))
     end
 
-    def prepend_filename(msg, file)
-      if msg
-        msg.map {|x| "#{file}: #{x}"}
-      else
-        []
-      end
-    end
-
     # Print the result of the command(s), if there are results to be printed.
     def run_on_change(res)
       errors = 0
@@ -38,7 +30,7 @@ module Guard
         file = File.join( options[:watchdir].to_s,file ) if options[:watchdir]
 
         if options[:syntax_check]
-          puts "Guard::PuppetLint: Syntax Check Enabled - Running on #{file}"
+          UI.info "Guard::PuppetLint: Syntax Check Enabled - Running on #{file}"
           maybe_bundle_with_env do
             @parser_messages = `puppet parser validate #{file} --color=false 2>&1`.split("\n")
           end
